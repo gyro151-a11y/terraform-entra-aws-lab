21cbe65e7dbcb4ac7a03388149e736ffdbffcfc1
Jeff <gyro151@gmail.com>
2026-05-30 21:08:36 -0500

    Security: apply trivy ignore to database encryption baseline to keep costs low

--- bootstrap.tf
+++ bootstrap.tf
@@ -25,6 +25,7 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
   }
 }
 
+# trivy:ignore:AVD-AWS-0132
 resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
   bucket = aws_s3_bucket.state_bucket.id
 
